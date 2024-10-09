24f35ec1e4a87f128f69e1b83f63ba8db0e08d89
Estudiante <Estudiante@pc26.lab1103.labos.fcen.uba.ar>
2024-10-09 17:30:27 -0300

    a

---
+++ Soluciones/Ejercicio1.py
@@ -0,0 +1,14 @@
+def imprimir_hola_mundo():
+    print("¡Hola mundo!")
+
+imprimir_hola_mundo()
+
+def imprimir_un_verso():
+    print("hola\ncomo vas")
+
+imprimir_un_verso()
+
+def raizDe2():
+    print(2**(1/2))
+
+raizDe2()
