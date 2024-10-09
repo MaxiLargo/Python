eb5c38f545c0998c2c8e765925af5770601ceedf
Estudiante <Estudiante@pc63.lab1105.labos.fcen.uba.ar>
2024-10-09 17:45:20 -0300

    Ejercicio1

--- Soluciones/Ejercicio1.py
+++ Soluciones/Ejercicio1.py
@@ -1,3 +1,5 @@
+import math
+
 def imprimir_hola_mundo():
     print("¡Hola mundo!")
 
@@ -9,6 +11,16 @@ def imprimir_un_verso():
 imprimir_un_verso()
 
 def raizDe2():
-    print(2**(1/2))
+    print(round(2**(1/2),4))
 
 raizDe2()
+
+def factorial_de_dos():
+    print(2)
+
+factorial_de_dos()
+
+def perimetro ():
+    return math.pi * 2
+
+    
