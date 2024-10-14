38b3db8ffcde6b3a8d93ca1700c92e6ce85f5c07
Maxi <maximilianonlargo@gmail.com>
2024-10-13 23:36:59 -0300

    Ejercicio 5

---
+++ Soluciones/Ejercicio5.py
@@ -0,0 +1,48 @@
+import Ejercicio2 as importado
+def devolver_el_doble_si_es_par(numero:int)->int:
+    res:int = 0
+    if importado.es_par(numero):
+        res = numero*2
+    else:
+        res = numero
+    return res
+
+print(devolver_el_doble_si_es_par(6))
+
+def devolver_el_doble_si_es_par(numero:int)->int:
+    res:int=0
+    if importado.es_par(numero):
+        res = numero
+    else:
+        res = numero + 1
+
+    return res 
+
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(numero):
+    res:int = 0
+    if numero % 3:
+        res = numero * 2
+    elif numero % 9:
+        res = numero * 3
+    else:
+        res = numero
+    return res 
+
+def lindo_nombre(nombre:str)->str:
+    res:str = ""
+    if len (nombre) >= 5:
+        res = "Tu nombre tiene muchas letras!"
+    else:
+        res = "Tu nombre tiene menos de 5 caracteres"
+    return res 
+
+def elRango(numero:int)->str:
+    res:str=""
+    if numero < 5:
+        res = "Menor a 5"
+    elif numero>=10 and numero<=20:
+        res = "Entre 10 y 20"
+    else:
+        res = "Mayor a 20"
+    return res
+
