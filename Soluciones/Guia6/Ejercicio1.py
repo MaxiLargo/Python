d6762ac754a095655e1c2106f4f7220131b877a7
Maxi <maximilianonlargo@gmail.com>
2024-10-19 20:12:50 -0300

    Guia 6

---
+++ Soluciones/Guia6/Ejercicio1.py
@@ -0,0 +1,24 @@
+import math
+
+def imprimir_hola_mundo():
+    print("¡Hola mundo!")
+
+imprimir_hola_mundo()
+
+def imprimir_un_verso():
+    print("hola\ncomo vas")
+
+imprimir_un_verso()
+
+def raizDe2():
+    print(round(2**(1/2),4))
+
+raizDe2()
+
+def factorial_de_dos():
+    print(2)
+
+factorial_de_dos()
+
+def perimetro ():
+    return math.pi * 2
