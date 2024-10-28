dbdf5524376c5e949ce0f1b5c8402a9219eff161
Estudiante <Estudiante@pc13.lab1104.labos.fcen.uba.ar>
2024-10-28 18:34:27 -0300

    Ejercicio4

---
+++ Soluciones/Guia7/Ejercicio4.py
@@ -0,0 +1,26 @@
+def nombreestudiantes():
+    res:list[str]=[]
+    while 1==1:
+        nombre:str = input("nombre alumno ")
+        if nombre == "":
+            return res
+        res.append(nombre)
+
+print(nombreestudiantes())
+    
+def cargasube():
+    res:list[(str,int)] = []
+    monedero:int=0
+    while 1==1:
+        operacion:str = input("Movimiento a hacer ")
+
+        if operacion == "C":
+            valor:int=int(input("Valor a cargar "))
+            res.append(("C",valor))
+        elif operacion == "D":
+            valor:int=int(input("Valor a descontar "))
+            res.append(("D",valor))
+        elif operacion == "X":
+            return res
+
+print(cargasube())
